127426991d1628101cb436b9697eec566a3807fb
fusethnd <fusethnd@gmail.com>
2024-03-11 12:07:12 +0700

    Initial Commit

---
+++ Spotify-charts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify-charts
+//
+//  Created by Thanadon Boontawee on 11/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify-charts/Spotify_chartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_chartsApp.swift
+//  Spotify-charts
+//
+//  Created by Thanadon Boontawee on 11/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_chartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
